cb301a99933053e5f27237a667b2dc5ead23ba6a
dimaswprakoso <dimaswp@gmail.com>
2019-05-05 18:10:19 +0200

    add logging

--- result_log.py
+++ result_log.py
@@ -1,10 +1,5 @@
 import mysql.connector
 
-# rst = {'test_type':'wup-lemma', 'pos':'n', 'wordsim_th':0.8, 'top_n':10, 'duration':808.5, 'no_items':356,
-#        'no_rec':346, 'no_labeled':312, 'tp':74, 'fp':282, 'tn':10, 'fn':10, 'precision':0.20786516853932585,
-#        'recall':0.23717948717948717, 'accuracy':0.23595505617977527, 'f1':0.2215568862275449}
-
-
 
 def log_result(log_data):
     db_user = 'root'
@@ -18,12 +13,7 @@ def log_result(log_data):
     sql = "INSERT INTO %s (test_time, %s) VALUES (NOW(), %s );" % ('result_log', columns, values)
 
     try:
-
         cursor.execute(sql)
         cnx.commit()
     except mysql.connector.Error as e:
         print("x Failed inserting data: {}\n".format(e))
-
-
-# log_result(rst)
-# print("done")
