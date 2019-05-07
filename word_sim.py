bb56a684e765f35f7f64649df56dbed800544a01
dimaswprakoso <dimaswp@gmail.com>
2019-05-07 10:32:52 +0200

    add logging

---
+++ word_sim.py
@@ -0,0 +1,80 @@
+import math
+import sys
+
+
+# constant
+ALPHA = 0.2
+# smoothing factor
+BETA = 0.45
+
+
+def length_dist(synset_1, synset_2):
+    """
+    Return a measure of the length of the shortest path in the semantic
+    ontology (Wordnet) between two synsets.
+    """
+    l_dist = sys.maxsize
+    if synset_1 is None or synset_2 is None:
+        return 0.0
+    if synset_1 == synset_2:
+        # if synset_1 and synset_2 are the same synset return 0
+        l_dist = 0.0
+    else:
+        wset_1 = set([str(x.name()) for x in synset_1.lemmas()])
+        wset_2 = set([str(x.name()) for x in synset_2.lemmas()])
+        if len(wset_1.intersection(wset_2)) > 0:
+            # if synset_1 != synset_2 but there is word overlap, return 1.0
+            l_dist = 1.0
+        else:
+            # just compute the shortest path between the two
+            l_dist = synset_1.shortest_path_distance(synset_2)
+            if l_dist is None:
+                l_dist = 0.0
+    # normalize path length to the range [0,1]
+    return math.exp(-ALPHA * l_dist)
+
+
+def hierarchy_dist(synset_1, synset_2):
+    """
+    Return a measure of depth in the ontology to model the fact that
+    nodes closer to the root are broader and have less semantic similarity
+    than nodes further away from the root.
+    """
+    h_dist = sys.maxsize
+    if synset_1 is None or synset_2 is None:
+        return h_dist
+    if synset_1 == synset_2:
+        # return the depth of one of synset_1 or synset_2
+        h_dist = max([x[1] for x in synset_1.hypernym_distances()])
+    else:
+        # find the max depth of least common subsumer
+        hypernyms_1 = {x[0]: x[1] for x in synset_1.hypernym_distances()}
+        hypernyms_2 = {x[0]: x[1] for x in synset_2.hypernym_distances()}
+        lcs_candidates = set(hypernyms_1.keys()).intersection(
+            set(hypernyms_2.keys()))
+        if len(lcs_candidates) > 0:
+            lcs_dists = []
+            for lcs_candidate in lcs_candidates:
+                lcs_d1 = 0
+                if lcs_candidate in hypernyms_1:
+                    lcs_d1 = hypernyms_1[lcs_candidate]
+                lcs_d2 = 0
+                if lcs_candidate in hypernyms_2:
+                    lcs_d2 = hypernyms_2[lcs_candidate]
+                lcs_dists.append(max([lcs_d1, lcs_d2]))
+            h_dist = max(lcs_dists)
+        else:
+            h_dist = 0
+    return ((math.exp(BETA * h_dist) - math.exp(-BETA * h_dist)) /
+            (math.exp(BETA * h_dist) + math.exp(-BETA * h_dist)))
+
+
+def li_similarity(synset1, synset2):
+    similarity = 0
+    similarity = length_dist(synset1, synset2) * hierarchy_dist(synset1, synset2)
+    return similarity
+
+
+
+
+
